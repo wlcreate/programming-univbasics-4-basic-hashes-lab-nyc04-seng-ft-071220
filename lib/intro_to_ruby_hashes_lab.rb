0ad5a5cd551afa7fde851d5892a93c97973b6ea6
Waverley Leung <waverley.nyc@gmail.com>
2020-07-17 13:38:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,28 +1,20 @@
 def new_hash
   # return an empty hash
-  empty_hash = {
-    
-  }
+  {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  breakfast = {
-    :eggs => "scrambled"
-  }
+ {:eggs => "scrambled"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  pioneer_hash = {
-    :name => "Grace Hopper"
-  }
+  {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  hash = {
-    :id => number
-  }
+  {:id => number}
 end
